7277edce70248efc08665b720c65cf818a6acacf
Hossam Youssef <hossam.mox@gmail.com>
2022-09-04 19:31:29 +0200

    Update vite.config.js

--- resources/stubs/theme/vite.config.js
+++ resources/stubs/theme/vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/theme.css', 'resources/js/theme.js'],
+            input: ['resources/sass/theme.scss', 'resources/js/theme.js'],
             refresh: true,
         }),
     ],
